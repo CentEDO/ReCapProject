d46f4a26bdbb63ba4f111c2a941cdaf7ebc96579
Edvin DAVULCU <edvindavulcu@gmail.com>
2021-03-05 18:53:11 +0300

    ReCapProject

--- DataAccess/Concrete/EntityFramework/EfCarDal.cs
+++ DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -1,7 +1,9 @@
 ﻿using DataAccess.Abstract;
 using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 
@@ -13,28 +15,46 @@ namespace DataAccess.Concrete.EntityFramework
         {
             using (ReCapProjectDatabaseContext context = new ReCapProjectDatabaseContext())
             {
-
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
             }
         }
 
         public void Delete(Car entity)
         {
-            throw new NotImplementedException();
+            using (ReCapProjectDatabaseContext context = new ReCapProjectDatabaseContext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
         }
 
         public Car Get(Expression<Func<Car, bool>> filter)
         {
-            throw new NotImplementedException();
+            using (ReCapProjectDatabaseContext context = new ReCapProjectDatabaseContext())
+            {
+                return context.Set<Car>().SingleOrDefault(filter);
+            }
         }
 
         public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
         {
-            throw new NotImplementedException();
+            using (ReCapProjectDatabaseContext context = new ReCapProjectDatabaseContext())
+            {
+                return filter == null ? context.Set<Car>().ToList() : context.Set<Car>().Where(filter).ToList();
+            }
         }
 
         public void Update(Car entity)
         {
-            throw new NotImplementedException();
+            using (ReCapProjectDatabaseContext context = new ReCapProjectDatabaseContext())
+            {
+                var updatedEntity = context.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
         }
     }
 }
